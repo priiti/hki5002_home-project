b9b65bd8cfbc31e35f21562f8c4d34c1213478b4
Priit Pärl <priitparl@gmail.com>
2017-04-17 15:57:43 +0300

    create port connection object

--- index.js
+++ index.js
@@ -12,28 +12,25 @@ var settings = {
 
 var connection = null;
 
-var apps = {};
+class Machine {
+	constructor(port, path) {
+		this.port = port;
+		this.app = express();
+		this.http = http.Server(this.app);
+		this.path = path;
+	}
 
-function startTicketMachine() {
-	apps.ticketMachine = {};
-	apps.ticketMachine.app = express();
-	apps.ticketMachine.http = http.Server(apps.ticketMachine.app);
-
-	apps.ticketMachine.app.use(express.static('public'));
-
-	apps.ticketMachine.app.get('/', (req, res) => {
-		res.sendFile(__dirname + '/public/ticket.html');
-	});
-
-	apps.ticketMachine.http.listen(settings.server.ticketPort, () => {
-		// if (err) {
-		// 	throw err;
-		// }
-
-		console.log("Ticket machine running on port %s.", settings.server.ticketPort);
-	});
-
+	startMachine() {
+		this.app.use(express.static('public'));
+		this.app.get('/', (req, res) => {
+			res.sendFile(__dirname + this.path);
+		});
+		this.http.listen(settings.server.ticketPort, () => {
+			console.log("Ticket machine running on port %s.", settings.server.ticketPort);
+		})
+	}
 }
 
-// We could start all the machines differently like this
-startTicketMachine();
+const myConnection = new Machine(3000, '/public/ticket.html');
+myConnection.startMachine();
+
