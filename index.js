7279ad33668b9f280d0a05446998583495921c47
Priit Pärl <priitparl@gmail.com>
2017-04-18 08:56:29 +0300

    running several servers

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ var socketio = require('socket.io');
 
 var settings = {
 	server: {
-		ticketPort: 3000,
-		infoTablePort: 3001,
-		doctorPort: 3002
+		ticket: 3000,
+		info: 3001,
+		doctor: 3002
 	}
 };
 
 var connection = null;
 
-class Machine {
+class Server {
 	constructor(port, path) {
 		this.port = port;
 		this.app = express();
@@ -20,17 +20,22 @@ class Machine {
 		this.path = path;
 	}
 
-	startMachine() {
+	startServer() {
 		this.app.use(express.static('public'));
 		this.app.get('/', (req, res) => {
 			res.sendFile(__dirname + this.path);
 		});
-		this.http.listen(settings.server.ticketPort, () => {
-			console.log("Ticket machine running on port %s.", settings.server.ticketPort);
+		this.http.listen(this.port, (err) => {
+			if (err) throw err;
+			console.log("Ticket Server running on port %s.", this.port);
 		})
 	}
 }
 
-const myConnection = new Machine(3000, '/public/ticket.html');
-myConnection.startMachine();
+const ticketStation = new Server(settings.server.ticket, '/public/ticket.html');
+const informationTable = new Server(settings.server.info, '/public/info.html');
+const doctorScreen = new Server(settings.server.doctor, '/public/doctor.html');
 
+ticketStation.startServer();
+informationTable.startServer();
+doctorScreen.startServer();
