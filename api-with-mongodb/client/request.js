539997eb797edd1fdfce90eaf5c21dc43bda909a
Priit Pärl <priitparl@gmail.com>
2017-05-01 13:08:39 +0300

    some functionality for ui, for testing api

--- api-with-mongodb/client/request.js
+++ api-with-mongodb/client/request.js
@@ -1,13 +1,27 @@
 'use strict';
 
+document.querySelector('.posts_btn')
+	.addEventListener('click', getAllPosts);
 
-fetch('http://localhost:3000/api/posts')
-	.then((resolve) => {
-		resolve.json()
-			.then((data) => {
-				console.log(data);
-			});
-	})
-	.catch((error) => {
-		console.log(`Some error with fetch ${error}`);
-	});
+function getAllPosts() {
+	fetch('http://localhost:3000/api/posts')
+		.then((resolve) => {
+			resolve.json()
+				.then((posts) => {
+					const ulElement = document.createElement('ul');
+					ulElement.setAttribute('class', 'list-group');	
+					for (let i = 0; i < posts.data.length; i++) {
+						console.log(posts.data[i]);
+						const liElement = document.createElement('li');
+						liElement.setAttribute('class', 'list-group-item');
+						liElement.innerText = posts.data[i].title;
+						ulElement.appendChild(liElement);
+					}
+					document.querySelector('#content')
+						.appendChild(ulElement);
+				});
+		})
+		.catch((error) => {
+			console.log(`Some error with fetch ${error}`);
+		});
+}
